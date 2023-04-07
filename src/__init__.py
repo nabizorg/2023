25d7c0b84494c0852c09fb55ac0eb2f1aca2fef0
div72 <60045611+div72@users.noreply.github.com>
2023-04-07 22:30:20 +0300

    all: use toml

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
+import os
 import sqlite3
+import tomllib
 
 import argon2.low_level
 from flask import abort, g, redirect, request, url_for, Flask, render_template
@@ -6,16 +8,15 @@ from flask import abort, g, redirect, request, url_for, Flask, render_template
 from . import nvi
 
 app = Flask(__name__)
-app.config["DATABASE_PATH"] = "votes.db"
-app.config["TCKN_SALT"] = "foobarbaz"
-app.config["VOTEES"] = "A;B"
+with open(os.environ.get("CONFIG_PATH", "config.toml"), "rb") as f:
+    app.config.update(tomllib.load(f))
 
 # We will access sqlite from a multithreaded context, so we need to ensure that the
 # sqlite3 library's thread safety option allows it. This also forces us to not use
 # a forking WSGI server like Gunicorn since the multithreaded assumption will break
 # in a non-nice way.
 assert sqlite3.threadsafety == 3
-db = sqlite3.connect(app.config["DATABASE_PATH"], check_same_thread=False)
+db = sqlite3.connect(app.config["database_path"], check_same_thread=False)
 with db:
     db.execute("""
 CREATE TABLE IF NOT EXISTS votes (
@@ -33,21 +34,20 @@ ARGON2_TIME_COST: int = 3
 ARGON2_MEMORY_COST: int = 65536
 ARGON2_PARALLELISM: int = 4
 ARGON2_HASH_LEN: int = 32
-SALT: bytes = app.config["TCKN_SALT"].encode()
+SALT: bytes = app.config["tckn_salt"].encode()
 def hash_tckn(tckn: str) -> bytes:
     return argon2.low_level.hash_secret_raw(secret=tckn.encode(), salt=SALT, time_cost=ARGON2_TIME_COST, memory_cost=ARGON2_MEMORY_COST,
                                             parallelism=ARGON2_PARALLELISM, hash_len=ARGON2_HASH_LEN, type=ARGON2_TYPE, version=ARGON2_VERSION)
 
 
-VOTEES = app.config["VOTEES"].split(";")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "GET":
         cur = db.cursor()
         cur.execute("SELECT vote, COUNT(vote) FROM votes GROUP BY vote;")
 
-        vote_counts: dict[str, int] = dict(map(lambda p: (VOTEES[p[0]], p[1]), cur.fetchall()))
-        return render_template("index.html", vote_counts=vote_counts)
+        vote_counts: dict[str, int] = dict(map(lambda p: (app.config["candidates"][p[0]], p[1]), cur.fetchall()))
+        return render_template("index.html", vote_counts=vote_counts, candidates=app.config["candidates"])
     elif request.method == "POST":
         # FIXME: Add captcha.
         try:
@@ -55,7 +55,7 @@ def index():
         except (KeyError, ValueError):
             abort(400)
 
-        if vote >= len(VOTEES):
+        if vote >= len(app.config["candidates"]):
             abort(400)
 
         # FIXME: Character check?
